0fa25a35c70709cfeb49daf7e6e855dfb17a8556
github username <bteng1226@gmail.com>
2018-12-12 16:26:56 -0500

    added controllers

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,46 @@
 class PicturesController < ApplicationController
+
+  def index
+    @pictures = Picture.all
+    render json: @pictures
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+    render json: @picture
+  end
+
+  def create
+    @picture = Picture.find_or_initialize_by(picture_params)
+
+    if @picture.save
+      render json: @picture, status: :accepted
+    else
+      render json: {errors: @picture.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update(picture_params)
+      render json: @picture, status: :accepted
+    else
+      render json: {errors: @picture.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+  end
+
+
+  private
+  def picture_params
+    params.require(:picture).permit!
+  end
+
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,44 @@
 class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    render json: @project
+  end
+
+  def create
+    @project = Project.find_or_initialize_by(project_params)
+
+    if @project.save
+      render json: @project, status: :accepted
+    else
+      render json: {errors: @project.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      render json: @project, status: :accepted
+    else
+      render json: {errors: @project.errors.full_messages}, status: :unprocessable_entity
+    end
+
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+  end
+
+  private
+  def project_params
+    params.require(:project).permit!
+  end
 end
